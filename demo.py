eee5dc094ad08d335d36c0ece278eb71a9c11406
noorbuchi <buchin@allegheny.edu>
2020-02-10 09:43:03 -0500

    Fix Style issues

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+"""FastAPI Demo designed to accept user cedentials and display GitHub info."""
 from fastapi import FastAPI
 from github import Github
 
@@ -6,8 +7,10 @@ app = FastAPI()
 current_user_token = "Error Not Available"
 current_user_account = Github()
 
+
 @app.get("/create_user")
 def create_user(user_token):
+    """Take token input from user and creates global GitHub object."""
     global current_user_token
     current_user_token = user_token
     global current_user_account
@@ -16,8 +19,10 @@ def create_user(user_token):
     statement = user.login + " created successfully!"
     return statement
 
+
 @app.get("/repo_list")
 def get_repo_list():
+    """List the users repositories' names."""
     # user_token = input("Enter your github token: ")
     # g = Github(user_token)
     repo_list = []
@@ -30,9 +35,10 @@ def get_repo_list():
     print("This user has " + str(count) + " repos")
     return repo_list
 
+
 @app.get("/branches_list")
 def get_repo_info(repo_index):
-    """ Please enter the number of the repository from the previous list to show information"""
+    """Please enter the number of the repository from the previous list to show information."""
     global current_user_account
     current_repo = current_user_account.get_user().get_repos()[int(repo_index)]
     # branches_list = current_repo.get_branches()
@@ -43,15 +49,24 @@ def get_repo_info(repo_index):
         # print(branch.name)
         count = count + 1
         branches_names.append(branch.name)
-    print("This user has " + str(count) + " branches in the " + current_repo.name + " repository")
+    print(
+        "This user has "
+        + str(count)
+        + " branches in the "
+        + current_repo.name
+        + " repository"
+    )
     return branches_names
 
+
 def main_method():
+    """Use to call previous functions in case of running through terminal."""
     userToken = str(input("Please enter your GitHub token: "))
     print(create_user(userToken))
     get_repo_list()
     repoIndex = int(input("Please enter an index number to show information:"))
     print(get_repo_info(repoIndex))
+
 
 choice = input("call main? (y = yes/ n = no)")
 if choice == "y":
