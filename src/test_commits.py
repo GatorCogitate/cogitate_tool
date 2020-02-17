0cf93ff2dca18a79457ddc14bd4dee08779a4c47
clussier <lussierc@allegheny.edu>
2020-02-17 18:49:02 -0500

    Add automation to get number of commits.

--- src/test_commits.py
+++ src/test_commits.py
@@ -3,13 +3,21 @@ from pydriller import RepositoryMining, GitRepository
 from array import *
 #from pydriller.domain.commit import ModificationType
 
-dt1 = datetime(2016,2,1)
-dt2 = datetime.now()
+commit_list = []
+commit_author_list = []
+
+for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
+    if commit.author.name not in commit_author_list:
+        commit_author_list.append(commit.author.name)
+        print("Adding author,", commit.author.name)
+    else:
+        pass
+
 list = ["clussier", "Spencer Huang", "Devin Ho", "Christian Lussier", "Lancaster Wu", "Juncheng Wu", "Gregory M. Kapfhammer"]
 # Class Roster: Caden Hinckley, Devin Spitalny, Tyler Pham, Cory Wiard,
 # Jordan Wilson, Anthony Baldeosingh, Caden Koscinski, Christopher Stephenson
 # Danny Reid, Jordan Byrne, Madelyn Kapfhammer, Megan Munzek,
-for i in list:
+for i in commit_author_list:
     b = 0
     c = 0
     for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
@@ -19,9 +27,11 @@ for i in list:
         # print("--- Commit Tag: ", commit.hash)
         # print("--- Commit Author: ", commit.author.name)
         # print("--- Commit Date: ", commit.author_date)
-        if commit.author.name in list:
+        if commit.author.name in commit_author_list:
             c = c + 1
     print(i, b)
+
+
 
 
     for modified_file in commit.modifications:
