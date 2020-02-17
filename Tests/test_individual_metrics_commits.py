5d6b32e33e8fbb9680c4f32b79e0b500c6999085
Jordan-A <jordanbyrne@Jordans-MacBook-Pro.local>
2020-02-17 17:07:02 -0500

    Refactored repository and added test file.

---
+++ Tests/test_individual_metrics_commits.py
@@ -0,0 +1,27 @@
+"""Tests to ensure that the functions in the demonstration work."""
+
+from demo import individual_metrics_commits
+
+
+def test_1():
+    """Test 1."""
+
+
+def test_2():
+    """Test 2."""
+
+
+def test_3():
+    """Test 3."""
+
+
+def test_4():
+    """Test 4."""
+
+
+def test_5():
+    """Test 5."""
+
+
+def test_6():
+    """Test 6."""
