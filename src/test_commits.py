38ba78245611b6fedc67efcab84bd2d3b2cabd1e
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-02-17 14:12:39 -0500

    Add author commit counter to test_commit

--- src/test_commits.py
+++ src/test_commits.py
@@ -1,18 +1,34 @@
 from datetime import datetime
 from pydriller import RepositoryMining, GitRepository
+from array import *
 #from pydriller.domain.commit import ModificationType
 
 dt1 = datetime(2016,2,1)
 dt2 = datetime.now()
-
+c = 0
+s = 0 
+d = 0
 
 for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
     print("COMMIT:")
     print("--- Commit Tag: ", commit.hash)
     print("--- Commit Author: ", commit.author.name)
     print("--- Commit Date: ", commit.author_date)
+    if 'clussier' in commit.author.name:
+        c = c + 1
+    if 'Spencer Huang' in commit.author.name:
+        s = s + 1
+    if 'Devin Ho' in commit.author.name:
+        d = d + 1
 
     for modified_file in commit.modifications:
         print("--- File Modified: ", modified_file.new_path)
     # for modified_file in commit.modifications:
     #     print(commit.has + "Modified file" + modified_file.filename)
+
+print()
+print("clussier total commits: ", c)
+print()
+print("Spencer Huang total commits: ", s)
+print()
+print("Devin Ho total commits: ", d)
