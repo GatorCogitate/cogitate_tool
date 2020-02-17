c27857d05a6ac2581fba5f4fd99471e2dad2ac4a
Jordan-A <jordanbyrne@Jordans-MacBook-Pro.local>
2020-02-17 18:57:31 -0500

    Added initial implementation of hash table.

--- im-commits-pygithub.py
+++ im-commits-pygithub.py
@@ -5,6 +5,10 @@ from pydriller.domain.commit import ModificationType
 from fastapi import FastAPI
 
 
+def output_hash_map():
+    """Method to print out the hash map."""
+
+
 def get_repo_commits_py_github():
     """Method to get commit information using pygithub"""
     user_token = input("Enter User Token: ")
@@ -14,17 +18,36 @@ def get_repo_commits_py_github():
     repo = user.get_repo("GatorCogitate/cogitate_tool")
     allcommits = repo.get_commits()
     all_branches = repo.get_branches()
+
     for branch in all_branches:
-        print(branch)
+        # Create hash map.
+        data_list = {}
         all_commits = repo.get_commits()
         all_comments = repo.get_comments()
+        print(branch)
+
         for commit in all_commits:
             print(commit.commit.author)
             print(commit.commit.author.date)
             print(commit.files)
             print(commit.commit.message)
             print("")
-            print("")
+
+            data_list[commit] = [
+                branch.name,
+                commit.commit.author,
+                commit.commit.author.date,
+                commit.files,
+                commit.commit.message,
+            ]
+
+            data_list[commit][0] += branch.name
+            data_list[commit][1] += commit.commit.author
+            data_list[commit][2] += commit.commit.author.date
+            data_list[commit][3] += commit.files
+            data_list[commit][4] += commit.commit.message
+
+    return data_list
 
 
 def main_method():
@@ -32,7 +55,7 @@ def main_method():
     """Use to call previous functions in case of running through terminal."""
 
     get_repo_commits_py_github()
-    get_repo_commits_py_driller()
+    print(data_list)
 
 
 main_method()
