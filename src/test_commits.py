baabda286aa072f5d32367d09e7384ebeb8c5300
clussier <lussierc@allegheny.edu>
2020-02-17 12:34:01 -0500

    Add code to get commit date from repo.

--- src/test_commits.py
+++ src/test_commits.py
@@ -5,13 +5,14 @@ from pydriller import RepositoryMining, GitRepository
 dt1 = datetime(2016,2,1)
 dt2 = datetime.now()
 
-for commit in RepositoryMining("https://github.com/GatorCogitate/cogitate_tool").traverse_commits():
+
+for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
     print("COMMIT:")
     print("--- Commit Tag: ", commit.hash)
     print("--- Commit Author: ", commit.author.name)
+    print("--- Commit Date: ", commit.author_date)
 
     for modified_file in commit.modifications:
         print("--- File Modified: ", modified_file.filename)
-
     # for modified_file in commit.modifications:
     #     print(commit.has + "Modified file" + modified_file.filename)
