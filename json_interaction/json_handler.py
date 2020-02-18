467ac154b29fa087b915ea407c0f66c12abb11aa
Chris Stephenson <stephensonc@allegheny.edu>
2020-02-18 13:39:37 -0500

    rename demo to json_interaction

--- json_interaction/json_handler.py
+++ json_interaction/json_handler.py
@@ -21,7 +21,7 @@ def get_dict_from_json_file(json_file_name):
     with open(os.path.join(data_path, json_file_name + ".json"), "r") as json_file:
         # In the open() function, "r" specifies read-only access
         user_data_dict = json.load(json_file)
-        # json.loads() converts a json-formatted string into a python dictionary
+        # json.load() converts a json file into a python dictionary
     return user_data_dict
 
 
@@ -30,7 +30,7 @@ def write_dict_to_json_file(user_data_dict, json_file_name):
     with open(os.path.join(data_path, json_file_name + ".json"), "w") as json_file:
         # In the open() function, "w" specifies write access
         json.dump(user_data_dict, json_file, indent=4)
-        # json.dumps() converts a dictionary into a json-formatted string.
+        # json.dump() converts a dictionary into a json-formatted string.
         # Specifying an indent does not alter the data itself, it only increases
         # readability in the json file.
 
