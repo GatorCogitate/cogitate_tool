ca39747025003f8034c12e76090f06f1633311d7
clussier <lussierc@allegheny.edu>
2020-02-17 19:08:44 -0500

    Implement code to get the number of commits modifying a test directory.

--- src/test_commits.py
+++ src/test_commits.py
@@ -13,31 +13,32 @@ for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExp
     else:
         pass
 
-list = ["clussier", "Spencer Huang", "Devin Ho", "Christian Lussier", "Lancaster Wu", "Juncheng Wu", "Gregory M. Kapfhammer"]
 # Class Roster: Caden Hinckley, Devin Spitalny, Tyler Pham, Cory Wiard,
 # Jordan Wilson, Anthony Baldeosingh, Caden Koscinski, Christopher Stephenson
 # Danny Reid, Jordan Byrne, Madelyn Kapfhammer, Megan Munzek,
 for i in commit_author_list:
     b = 0
     c = 0
+    d = 0
     for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
+        count = 0
         if commit.author.name in i:
             b = b + 1
-        # print("COMMIT:")
-        # print("--- Commit Tag: ", commit.hash)
-        # print("--- Commit Author: ", commit.author.name)
-        # print("--- Commit Date: ", commit.author_date)
+            for modified_file in commit.modifications:
+                file_path = modified_file.new_path
+                if count is 0:
+                    if file_path:
+                        if "test" in file_path:
+                            print("Found someone who modified tests in file", commit.author.name, file_path, commit.hash)
+                            d += 1
+                            count = 1
+                        else:
+                            pass
         if commit.author.name in commit_author_list:
             c = c + 1
+
     print(i, b)
-
-
-
-
-    for modified_file in commit.modifications:
-        print("--- File Modified: ", modified_file.new_path)
-    # for modified_file in commit.modifications:
-    #     print(commit.has + "Modified file" + modified_file.filename)
+    print("-- Testing commits by", i, d)
 
 print()
 print("total commits: ", c)
