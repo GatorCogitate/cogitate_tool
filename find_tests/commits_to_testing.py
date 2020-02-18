9512ede2e7be390d25296f26b8229d0005f03c29
clussier <lussierc@allegheny.edu>
2020-02-18 14:53:39 -0500

    Implement code to allow user to chose repository.

--- find_tests/commits_to_testing.py
+++ find_tests/commits_to_testing.py
@@ -8,17 +8,22 @@
 from datetime import datetime
 from pydriller import RepositoryMining, GitRepository
 
+user_repo = input("Enter the link to your chosen GitHub repository: ")
+
 # accesses users
 commit_author_list = []
 # accesses github repo to calculate users commits
 for commit in RepositoryMining(
-    "https://github.com/lussierc/simplePerformanceExperimentsJava"
+    user_repo
 ).traverse_commits():
     if commit.author.name not in commit_author_list:
         commit_author_list.append(commit.author.name)
         print("Adding author,", commit.author.name)
     else:
         pass
+
+
+print()
 # Calculates the total commits like author, test and general and connecting
 # to the chosen repo
 for author_name in commit_author_list:
@@ -39,7 +44,7 @@ for author_name in commit_author_list:
                     if file_path:
                         if "test" in file_path:
                             print(
-                                "Found someone who modified tests in file",
+                                "Found someone who modified tests in file: ",
                                 # will calculate the modifications to the test
                                 commit.author.name,
                                 file_path,
@@ -53,9 +58,10 @@ for author_name in commit_author_list:
             total_commit_count = total_commit_count + 1
     # Print statements that release the calculations of the declared variables
     print(author_name, "'s Total commits: ", author_commit_count)
-    print("-- Testing commits by", author_name, total_test_commit_count)
+    print("-- Testing commits by", author_name, ":", total_test_commit_count)
     percentage_covered = (total_test_commit_count / author_commit_count) * 100
-    print("-- Percentage of Commits Going to Testing:", percentage_covered)
+    print("-- Percentage of Commits Going to Testing:", percentage_covered, "%")
+    print("\n\n")
 
 print()
-print("total commits: ", total_commit_count)
+print("Total commits in repository: ", total_commit_count)
