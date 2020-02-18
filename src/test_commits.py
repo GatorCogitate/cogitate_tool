dd43a7de3bfd1e1317e6c9bdcdc054a885127263
devin spitalny <devinspitalny@devins-MacBook-Air.local>
2020-02-18 11:51:39 -0500

    Commented the main file to give an understanding of the code

--- src/test_commits.py
+++ src/test_commits.py
@@ -9,22 +9,25 @@
 from datetime import datetime
 from pydriller import RepositoryMining, GitRepository
 from array import *
-
+# accesses users
 commit_author_list = []
-
+# accesses github repo to calculate users commits
 for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
     if commit.author.name not in commit_author_list:
         commit_author_list.append(commit.author.name)
         print("Adding author,", commit.author.name)
     else:
         pass
-
+# Calculates the total commits like author, test and general and connecting
+# to the chosen repo
 for author_name in commit_author_list:
     author_commit_count = 0
     total_commit_count = 0
     total_test_commit_count = 0
     for commit in RepositoryMining("https://github.com/lussierc/simplePerformanceExperimentsJava").traverse_commits():
         count = 0
+        # Connects the author name to the amount of commits made by user the
+        # clear path to the repo
         if commit.author.name in author_name:
             author_commit_count = author_commit_count + 1
             for modified_file in commit.modifications:
@@ -32,14 +35,16 @@ for author_name in commit_author_list:
                 if count is 0:
                     if file_path:
                         if "test" in file_path:
-                            print("Found someone who modified tests in file", commit.author.name, file_path, commit.hash)
+                            print("Found someone who modified tests in file",
+                            # will calculate the modifications to the test
+                            commit.author.name, file_path, commit.hash)
                             total_test_commit_count += 1
                             count = 1
                         else:
                             pass
         if commit.author.name in commit_author_list:
             total_commit_count = total_commit_count + 1
-
+    # Print statements that release the calculations of the declared variables
     print(author_name, "'s Total commits: ", author_commit_count)
     print("-- Testing commits by", author_name, total_test_commit_count)
     percentage_covered = (total_test_commit_count / author_commit_count) * 100
