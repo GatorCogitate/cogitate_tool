8650d904a9aedcb0354fa08c4d578ef8966bb7a8
johnsc1 <johnsc@allegheny.edu>
2020-02-17 21:40:55 -0500

    added docstrings and put the code into a method

--- driller.py
+++ driller.py
@@ -1,8 +1,10 @@
+''' Uses Pydriller to extract information from Github.'''
 from git import Repo
 from pydriller import RepositoryMining
 
-# method to receive a URL from user
-print("Please enter the URL of your terminal:")
-repo = input()
-# use URL input as parameter for RepositoryMining
-miner = RepositoryMining(path_to_repo=repo)
+def find_repositories():
+    ''' Recieves a URL from the user to locate a Github repository. '''
+    print("Please enter the URL of your terminal:")
+    repo = input()
+    # use URL input as parameter for RepositoryMining
+    miner = RepositoryMining(path_to_repo=repo)
