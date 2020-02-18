ca2b43040850d0118e260601db90409149f28c03
johnsc1 <johnsc@allegheny.edu>
2020-02-17 23:58:55 -0500

    attempted a test case

--- test_cogitate_tool.py
+++ test_cogitate_tool.py
@@ -1,8 +1,8 @@
 from cogitate_tool import CL_interface
 
 
-def test_answers_dict_():
+def test_answers_dict():
     """ Checks that the answers_dict populates with user input. """
-    assert len(cogitate_tool.answers) == 0
+    assert len(CL_interface.answers) == 0
     congitate_tool.CL_interface()
-    assert len(cogitate_tool.answers) < 0
+    assert len(CL_interface.answers) < 0
