6d2967b7d333fcbc0fdfa43a3564bb8550f37380
Jordan-A <jordanbyrne@Jordans-MacBook-Pro.local>
2020-02-18 00:20:45 -0500

    Implemented feedback from pull request #33.

--- pygithub/im_commits_pygithub.py
+++ pygithub/im_commits_pygithub.py
@@ -1,17 +1,15 @@
 """Demo designed to find and store information about PyGithub."""
 from github import Github
-from pydriller import RepositoryMining, GitRepository
-from pydriller.domain.commit import ModificationType
-from fastapi import FastAPI
 
 test_dict = {}
-def output_hash_map(dictionary):
+
+
+def print_dictionary(dictionary):
     """Method to print out the hash map."""
     # print headings
     print("Branch", "\t Author", "\t Time", "\t Files", "\t Message")
     # prints hashmap content
     # for key in dictionary:
-
 
     for key, _ in dictionary.items():
         print(key)
@@ -20,26 +18,24 @@ def output_hash_map(dictionary):
                 print("\t" + str(item) + "\t")
             # print("\t" + str(item))
 
-def get_repo_commits_py_github():
+
+def get_repo_commits_pygithub(user):
     """Method to get commit information using pygithub"""
-    user_token = input("Enter User Token: ")
-    user = Github(user_token)
-    print(user.get_user().login)
+
     username = user.get_user().login
+    print(username)
     repo = user.get_repo("GatorCogitate/cogitate_tool")
-    allcommits = repo.get_commits()
     all_branches = repo.get_branches()
 
     branches = {}
 
     for branch in all_branches:
-        # Create hash map.
+        # Create an empty list.
         data_list = list()
         all_commits = repo.get_commits()
         all_comments = repo.get_comments()
         print(branch)
         for commit in all_commits:
-            # commit_info = ()
             commit_info = (
                 branch.name,
                 commit.commit.author,
@@ -53,14 +49,9 @@ def get_repo_commits_py_github():
     return branches
 
 
-def main_method():
-    # pylint: disable=input-builtin
-    """Use to call previous functions in case of running through terminal."""
-
-    dictionary = get_repo_commits_py_github()
-    test_dict = dictionary
-
-    output_hash_map(dictionary)
-
 if __name__ == "__main__":
-    main_method()
+    user_token = input("Enter User Token: ")
+    user = Github(user_token)
+
+    dictionary = get_repo_commits_pygithub(user)
+    output_hash_map(dictionary)
