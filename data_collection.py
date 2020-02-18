753e854081d62159b0e60b9029a9a5bedb5c2d91
Pedro Carmo <carmop@aldenv169.allegheny.edu>
2020-02-17 19:55:00 -0500

    Add comments

--- data_collection.py
+++ data_collection.py
@@ -1,9 +1,16 @@
+
 from pydriller import RepositoryMining
+
 import pprint
 
+# create a dictionary that will hold the email of the repo members as keys
+# and the contents of Commit object as values
 author_dict = {}
 
+#indicate path for repository by pluggin in the URL of repo (must be public)
 repo_path = "https://github.com/GatorCogitate/cogitate_tool"
+
+#loop to turn the each Commits object into the values of the dictionary `author_dict`
 for commit in RepositoryMining(repo_path).traverse_commits():
     if commit.author.email not in author_dict.keys():
         author_dict[commit.author.email] = [commit]
@@ -12,13 +19,13 @@ for commit in RepositoryMining(repo_path).traverse_commits():
 
 # pprint.pprint(commit_lst)
 # for commit in commit_lst:
-#    if commit["author"] not in author_dict.keys():
-#        author_dict[commit["author"]] = [commit["commit"]]
-#    else:
-#        author_dict[commit["author"]].append(commit["commit"])
+#     if commit["author"] not in author_dict.keys():
+#         author_dict[commit["author"]] = [commit["commit"]]
+#     else:
+#         author_dict[commit["author"]].append(commit["commit"])
 
 pprint.pprint(author_dict)
 
-# for commit in author_dict["cklima616"]:
-#    print(commit.committer.email)
-#    print(commit.committer.name)
+# for commit in author_dict["Thomas Cassidy"]:
+#     print(commit.committer.email)
+#     print(commit.committer.name)
