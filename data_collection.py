3ed81914ecb66975da82c0d9196c22d2b3076dff
johnsc1 <johnsc@allegheny.edu>
2020-02-17 22:14:27 -0500

    moved comments to correct line in code

--- data_collection.py
+++ data_collection.py
@@ -1,19 +1,18 @@
-''' Collects commit data for a single user. '''
+""" Collects commit data for a single user. """
 from pydriller import RepositoryMining
 
 import pprint
 
-# create a dictionary that will hold the email of the repo members as keys
-# and the contents of Commit object as values
 
 def collect_data():
-    ''' Creates a dictionary of commit objects for a single user. '''
+    """ Creates a dictionary of commit objects for a single user. """
+    # holds email of repo members as keys, contents of commit object as values
     author_dict = {}
 
-    #indicate path for repository by pluggin in the URL of repo (must be public)
+    # indicate path for repository by pluggin in the URL of repo (must be public)
     repo_path = "https://github.com/GatorCogitate/cogitate_tool"
 
-    #loop to turn the each Commits object into the values of the dictionary `author_dict`
+    # loop to turn the each Commits object into the values of the dictionary `author_dict`
     for commit in RepositoryMining(repo_path).traverse_commits():
         if commit.author.email not in author_dict.keys():
             author_dict[commit.author.email] = [commit]
