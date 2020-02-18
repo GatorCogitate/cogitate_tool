9677b19a73d9b12214942a907aec5396a9e23b96
johnsc1 <johnsc@allegheny.edu>
2020-02-17 22:09:57 -0500

    clarified docstrings and added parameters to the find_repositories fucntion instead of taking user input in the function

--- driller.py
+++ driller.py
@@ -1,10 +1,10 @@
-''' Uses Pydriller to extract information from Github.'''
+""" Uses Pydriller to extract information from Github."""
 from git import Repo
 from pydriller import RepositoryMining
 
-def find_repositories():
-    ''' Recieves a URL from the user to locate a Github repository. '''
-    print("Please enter the URL of your terminal:")
-    repo = input()
-    # use URL input as parameter for RepositoryMining
+
+def find_repositories(repo):
+    """ Locates a Github repository with the URL provided by the user. """
+    # ask the user for a URL of a Github repository
     miner = RepositoryMining(path_to_repo=repo)
+    return miner
