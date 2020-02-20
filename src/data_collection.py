1453d0ec163a1824024c196faf628c8a626dc13a
Pedro Carmo <carmop@aldenv177.allegheny.edu>
2020-02-20 18:30:18 -0500

    Add new function

--- src/data_collection.py
+++ src/data_collection.py
@@ -3,20 +3,55 @@ from pydriller import RepositoryMining
 
 import pprint
 
+try:
+    import git
+except:
+    pass
 
-def collect_commits():
+# indicate path for repository by pluggin in the URL of repo (must be public)
+repo_path = "https://github.com/GatorCogitate/cogitate_tool"
+
+
+def collect_commits_user_email_key(repo):
     # TODO: edit the method to take in the URL for the repo
     """ Creates a dictionary of commit objects for a single user. """
     # holds email of repo members as keys, contents of commit object as values
     author_dict = {}
 
-    # indicate path for repository by pluggin in the URL of repo (must be public)
-    repo_path = "https://github.com/GatorCogitate/cogitate_tool"
 
     # loop to turn the each Commits object into the values of the dictionary `author_dict`
-    for commit in RepositoryMining(repo_path).traverse_commits():
+    for commit in RepositoryMining(repo).traverse_commits():
         if commit.author.email not in author_dict.keys():
             author_dict[commit.author.email] = [commit]
         else:
             author_dict[commit.author.email].append(commit)
     return author_dict
+
+
+def collect_commits_hash(repo):
+
+    commit_list = []
+
+    for commit in RepositoryMining(repo).traverse_commits():
+
+        # print(commit)
+        # print(commit.in_main_branch)
+        # commit_list.append(
+        # {"hash":commit.hash,
+        # "author_msg":commit.msg,
+        # "author_name":commit.author.name,
+        # "author_email":commit.author.email,
+        # "author_date":commit.author_date,
+        # "in_main_branch":commit.in_main_branch,
+        # # "merge":commit.merge,
+        # # # "change_type":commit.modifications.change_type,
+        # # "added":commit.modifications.added,
+        # # "removed":commit.modifications.removed,
+        # # "nloc":commit.modifications.nloc,
+        # # "complexity":commit.modifications.complexity,
+        # # "methods":commit.modifications.methods,
+        # })
+
+    return commit_list
+
+print (collect_commits_hash(repo_path))
