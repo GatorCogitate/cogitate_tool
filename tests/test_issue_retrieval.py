6af3487798483ca69d04e9ce35785e97f9ee4951
Caden Koscinski <koscinskic@allegheny.edu>
2020-02-23 17:50:23 -0500

    Updated test_issue_retrieval.py

--- tests/test_issue_retrieval.py
+++ tests/test_issue_retrieval.py
@@ -1,4 +1,5 @@
 from src import issue_retrieval
+from json_interaction import json_handler
 from github import Github
 
 # As of the current state, these tests require a token to function
@@ -7,74 +8,50 @@ from github import Github
 # collaboration provided an alternative test procedure is possible
 #
 # Remove your token from this file after demonstration
-temp_token = "REDACTED"
-temp_repo = "GatorCogitate/cogitate_tool"
-temp_contributors = {"stephensonc": [], "koscinskic": [], "schultzh": []}
+token = "9fd3989585322606a5107412bb9c2ffa82ad25ee"
+repository_name = "GatorCogitate/cogitate_tool"
+contributor_data = json_handler.get_dict_from_json_file("contributor_data")
 
 
 def test_retrieve_issue_data_retrieves_issues():
     """Test to ensure all issues are associated with the correct contributor"""
 
     # Refer to concerns above regarding future development
-    global temp_token
-    global temp_repo
-    global temp_contributors
-    ghub = Github(temp_token)
-    repo = ghub.get_repo(temp_repo)
+    global token
+    global repository
+    global contributor_data
+    ghub = Github(token)
+    repository = ghub.get_repo(repository_name)
+    issues = repository.get_issues(state = "all")
 
-    # Establishing test parameters
-    issues = repo.get_issues()
-    issues_contribution = {}
-    for username in temp_contributors:
-        issues_contribution[username] = []
-    issues_contribution = issue_retrieval.retrieve_issue_data(issues, issues_contribution)
+    contributor_data = issue_retrieval.retrieve_issue_data(issues, contributor_data)
 
     contributor_found = False
 
-    for username in issues_contribution:
-        for issueID in issues_contribution[username]:
-            # We are only checking for non-pr issues
-            assert repo.get_issue(number = issueID).pull_request == None
+    for username in contributor_data:
+
+        for issueID in contributor_data[username]["issues_opened"]:
+            assert repository.get_issue(number = issueID).pull_request == None
+            assert repository.get_issue(number = issueID).user.login == username
+
+        for issueID in contributor_data[username]["issues_commented"]:
+            assert repository.get_issue(number = issueID).pull_request == None
             contributor_found = False
-            # No need to search comments if the contributer issued the issue
-            if repo.get_issue(number = issueID).user.login == username:
-                contributor_found = True
             while contributor_found is False:
-                for comment in repo.get_issue(number = issueID).get_comments():
+                for comment in repository.get_issue(number = issueID).get_comments():
                     if comment.user.login == username:
                         contributor_found = True
             assert contributor_found is True
 
+        for issueID in contributor_data[username]["pull_requests_opened"]:
+            assert repository.get_issue(number = issueID).pull_request != None
+            assert repository.get_issue(number = issueID).user.login == username
 
-def test_retrieve_pr_data_retrieves_prs():
-    """Test to ensure all pull requests are associated with the correct contributor"""
-
-    # Refer to concerns above regarding future development
-    global temp_token
-    global temp_repo
-    global temp_contributors
-    ghub = Github(temp_token)
-    repo = ghub.get_repo(temp_repo)
-
-    # Establishing test parameters
-    issues = repo.get_issues()
-    pr_contribution = {}
-    for username in temp_contributors:
-        pr_contribution[username] = []
-    pr_contribution = issue_retrieval.retrieve_pr_data(issues, pr_contribution)
-
-    contributor_found = False
-
-    for username in pr_contribution:
-        for issueID in pr_contribution[username]:
-            # We are only checking for pull requests
-            assert repo.get_issue(number = issueID).pull_request != None
+        for issueID in contributor_data[username]["pull_requests_commented"]:
+            assert repository.get_issue(number = issueID).pull_request != None
             contributor_found = False
-            # No need to search comments if the contributer issued the pull request
-            if repo.get_issue(number = issueID).user.login == username:
-                contributor_found = True
             while contributor_found is False:
-                for comment in repo.get_issue(number = issueID).get_comments():
+                for comment in repository.get_issue(number = issueID).get_comments():
                     if comment.user.login == username:
                         contributor_found = True
             assert contributor_found is True
