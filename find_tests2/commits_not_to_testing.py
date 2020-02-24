396ca67cfa95c2a135d56c8bf7d7d0e1e7c0f8c0
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-02-24 00:47:14 -0500

    Add code to find non test commit count

--- find_tests2/commits_not_to_testing.py
+++ find_tests2/commits_not_to_testing.py
@@ -84,12 +84,13 @@ def get_commit_info(commit_author_list, user_repo):
                 total_commit_count = total_commit_count + 1
         # Print statements that release the calculations of the declared variables
         print(author_name, "'s Total commits: ", author_commit_count)
-        print("-- Testing commits by", author_name, ":", total_test_commit_count)
+        non_test_commit_count = total_commit_count - total_test_commit_count
+        print("-- Non Testing commits by", author_name, ":", non_test_commit_count)
         try:
-            percentage_covered = (total_test_commit_count / author_commit_count) * 100
+            percentage_covered = (non_test_commit_count / author_commit_count) * 100
         except:
             percentage_covered = 0
-        print("-- Percentage of Commits Going to Testing:", percentage_covered, "%")
+        print("-- Percentage of Commits not going to Testing:", percentage_covered, "%")
         print("\n\n")
 
 
