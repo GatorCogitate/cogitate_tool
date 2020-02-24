94a68accb2935b4e7e239288be53d5ddee331f26
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-02-24 00:44:25 -0500

    Add function that looks at commits to test

--- find_tests2/commits_not_to_testing.py
+++ find_tests2/commits_not_to_testing.py
@@ -3,6 +3,7 @@
 
 from pydriller import RepositoryMining, GitRepository  # import necessary libraries
 
+
 def get_repo_authors(user_repo):
     """Accesses the remote repo and puts author names in a list."""
 
@@ -23,6 +24,7 @@ def get_repo_authors(user_repo):
         print(author_name)
 
     return commit_author_list
+
 
 def single_multi_author_choice(commit_author_list):
     """Offers user the choice of looking at all or one specific author(s)."""
@@ -45,6 +47,51 @@ def single_multi_author_choice(commit_author_list):
     return commit_author_list
 
 
+def get_commit_info(commit_author_list, user_repo):
+    """Retrieves information from the repostory relating to commits to testing."""
+
+    # Calculates the total commits like author, test and general and connecting
+    # to the chosen repo
+    for author_name in commit_author_list:
+        author_commit_count = 0
+        total_commit_count = 0
+        total_test_commit_count = 0
+        for commit in RepositoryMining(
+            user_repo, only_authors=commit_author_list
+        ).traverse_commits():
+            count = 0
+            # Connects the author name to the amount of commits made by user the
+            # clear path to the repo
+            if commit.author.name in author_name:
+                author_commit_count = author_commit_count + 1
+                for modified_file in commit.modifications:
+                    file_path = modified_file.new_path
+                    if count is 0:
+                        if file_path:
+                            if "test" in file_path:  # sees if the commit was to testing
+                                # print(
+                                #     "Found someone who modified tests: ",
+                                #     # will calculate the modifications to the test
+                                #     commit.author.name,
+                                #     file_path,
+                                #     commit.hash,
+                                # )
+                                total_test_commit_count += 1
+                                count = 1
+                            else:
+                                pass
+            if commit.author.name in commit_author_list:
+                total_commit_count = total_commit_count + 1
+        # Print statements that release the calculations of the declared variables
+        print(author_name, "'s Total commits: ", author_commit_count)
+        print("-- Testing commits by", author_name, ":", total_test_commit_count)
+        try:
+            percentage_covered = (total_test_commit_count / author_commit_count) * 100
+        except:
+            percentage_covered = 0
+        print("-- Percentage of Commits Going to Testing:", percentage_covered, "%")
+        print("\n\n")
+
 
 def main():
     """Driver function. Runs all other necessary functions."""
@@ -52,5 +99,6 @@ def main():
     user_repo = input("Enter the link to your chosen GitHub repository: ")
     commit_author_list = get_repo_authors(user_repo)
     commit_author_list = single_multi_author_choice(commit_author_list)
+    get_commit_info(commit_author_list, user_repo)
 
 main()  # call the main function; run the program
