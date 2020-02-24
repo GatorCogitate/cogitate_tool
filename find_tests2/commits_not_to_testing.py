a1ec62e83cb692e1dd7859de7114bc6208da7602
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-02-24 00:35:34 -0500

    Add function to get author name and store it

--- find_tests2/commits_not_to_testing.py
+++ find_tests2/commits_not_to_testing.py
@@ -3,8 +3,30 @@
 
 from pydriller import RepositoryMining, GitRepository  # import necessary libraries
 
+def get_repo_authors(user_repo):
+    """Accesses the remote repo and puts author names in a list."""
+
+    # accesses users
+    commit_author_list = []
+    # accesses github repo to calculate users commits
+    for commit in RepositoryMining(user_repo).traverse_commits():
+        if commit.author.name not in commit_author_list:
+            commit_author_list.append(commit.author.name)
+            print("Adding author,", commit.author.name)
+        else:
+            pass
+
+    print("-- Repo Authors:")
+    for author_name in commit_author_list:
+        print(author_name)
+
+    return commit_author_list
+
+
+
 def main():
     """Driver function. Runs all other necessary functions."""
+
     user_repo = input("Enter the link to your chosen GitHub repository: ")
 
 main()  # call the main function; run the program
