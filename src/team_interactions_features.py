a8ecdbbedfdd2f32fd1c0e397a816e2e06a113cb
StringerJ <stringerj@allegheny.edu>
2020-02-24 16:57:52 -0500

    Added basic definition of a fucntion to check whether a person is a code hoarder along with data for the function to parse through.

--- src/team_interactions_features.py
+++ src/team_interactions_features.py
@@ -1,6 +1,35 @@
 """ Features to measure code hoarding and common collaborations in branches. """
 
-""" Method to determine code hoarders. """
+
+def code_hoarders():
+    """ Method to determine code hoarders. """
+    code_hoarder_list = []
+    repo_name = "cogitate"
+    branch_1 = ["user1", "user2", "user3", "user4"]
+    branch_2 = ["user1", "user2", "user4"]
+    branch_3 = ["user4"]
+    branch_4 = ["user3", "user1"]
+    branch_5 = ["user2"]
+    branch_dict = {
+                   "branch1": branch_1,
+                   "branch2": branch_2,
+                   "branch3": branch_3,
+                   "branch4": branch_4,
+                   "branch5": branch_5
+                   }
+    for branch_name, user_list in branch_dict.items():
+        if len(user_list) == 1:
+            code_hoarder_list.append(user_list[0] + " is a code hoarder," +
+                                     " since they were the only " +
+                                     "contributor to: " + branch_name)
+        else:
+            pass
+    return code_hoarder_list
+
+
+c_h_list = code_hoarders()
+for value in c_h_list:
+    print(value)
 
 """ Method to determine frequent collaborators in a team project. """
 # will add a feature to have user define this value
--- tests/test_driller.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from src import data_collection
-
-
-def test_collect_commits_user_email_key():
-    pass
