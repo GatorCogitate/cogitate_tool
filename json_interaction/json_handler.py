b7d9e29fbf6e1ef6870aa8554c02bf1398320845
Chris Stephenson <stephensonc@allegheny.edu>
2020-02-24 15:48:57 -0500

    Removed the temporary main function from json_handler.py Additionally, it has been updated to use default arguments instead of a global variable for the data path

--- json_interaction/json_handler.py
+++ json_interaction/json_handler.py
@@ -2,27 +2,8 @@
 import json
 import os
 
-data_path = "./data/"
 
-
-def main():
-    """Pull data from a provided json, and write it back to the json."""
-    print("Retrieved data from json: \n")
-    user_data_dict = get_dict_from_json_file("demofile")
-    for username in user_data_dict:
-        print(username + ":", user_data_dict[username], "\n")
-    write_dict_to_json_file(user_data_dict, "demofile")
-    with open(os.path.join(data_path, "demofile.json"), "r") as json_file:
-        print("Contents of json file: \n", json_file.read())
-    new_user = {"testuser": {"commits": [], "issues": [], "pull_requests": []}}
-    print("\nAdding testuser to dictionary\n")
-    add_user_to_users_dictionary(user_data_dict, new_user)
-    write_dict_to_json_file(user_data_dict, "demofile")
-    with open(os.path.join(data_path, "demofile.json"), "r") as json_file:
-        print("Contents of json file: \n", json_file.read())
-
-
-def get_dict_from_json_file(json_file_name):
+def get_dict_from_json_file(json_file_name, data_path="./data/"):
     """Populate and return a dictionary of all the data in a specified json file."""
     with open(os.path.join(data_path, json_file_name + ".json"), "r") as json_file:
         # In the open() function, "r" specifies read-only access
@@ -31,7 +12,7 @@ def get_dict_from_json_file(json_file_name):
     return user_data_dict
 
 
-def write_dict_to_json_file(user_data_dict, json_file_name):
+def write_dict_to_json_file(user_data_dict, json_file_name, data_path="./data/"):
     """Overwrite specified json file with data from a given dictionary."""
     with open(os.path.join(data_path, json_file_name + ".json"), "w") as json_file:
         # In the open() function, "w" specifies write access
@@ -44,7 +25,3 @@ def write_dict_to_json_file(user_data_dict, json_file_name):
 def add_user_to_users_dictionary(user_data_dict, to_add):
     """Append data to the users dictionary"""
     user_data_dict.update(to_add)
-
-
-if __name__ == "__main__":
-    main()
