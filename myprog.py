8498f580775da37759a270ed6d73aab0c36a6cca
clussier <lussierc@allegheny.edu>
2020-02-24 16:59:38 -0500

    Add code churn functionality for one author

--- myprog.py
+++ myprog.py
@@ -1,14 +1,62 @@
 """Checks that the compute_tf_monolith.py produces the expected output."""
 
+from pydriller import RepositoryMining, GitRepository  # import necessary libraries
+
 import shlex
 import subprocess
 import sys
 
+
+def get_repo_authors(user_repo):
+    """Accesses the remote repo and puts author names in a list."""
+
+    # accesses users
+    commit_author_list = []
+    # accesses github repo to calculate users commits
+    for commit in RepositoryMining(user_repo).traverse_commits():
+        if commit.author.name not in commit_author_list:
+            commit_author_list.append(commit.author.name)
+            print("Adding author,", commit.author.name)
+        else:
+            pass
+
+    print("-- Repo Authors:")
+    for author_name in commit_author_list:
+        print(author_name)
+
+    return commit_author_list
+
+
+def single_multi_author_choice(commit_author_list):
+    """Offers user the choice of looking at all or one specific author(s)."""
+
+    user_author_choice = int(
+        input(
+            "-- Would you like to look at (1) all authors or a (2) specific author?: "
+        )
+    )
+    if user_author_choice == 1:
+        pass
+    elif user_author_choice == 2:
+        # Looks for specific author contributions
+        specific_author = input("-- Enter author name:")
+        if specific_author in commit_author_list:
+            commit_author_list = [specific_author]
+    else:
+        print("Invalid choice!")
+
+    return commit_author_list
+
 if __name__ == "__main__":
+    user_repo = "~/cs203s2020/labs/cogitate_tool"
+    authors = get_repo_authors(user_repo)
+    commit_author_list = single_multi_author_choice(authors)
     # assume that all of the checks run correctly and prove otherwise
     exit_code = 0
     # defines the command that will call the check_compute_tf_monolith.py
-    command = 'dml gstats authorchurnmeta --author "MaddyKapfhammer"'
+    author_name = commit_author_list[0]
+    base_command = 'dml gstats authorchurnmeta --author '
+    command = base_command + author_name
 
     # tokenize this command so that subprocess can accept each of its parts
     tokenized_command = shlex.split(command)
