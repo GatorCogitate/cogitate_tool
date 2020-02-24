8e3b2516f147a2dd6721439ca1ca872fb2fd5d2a
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-02-24 00:34:23 -0500

    Add main function and call it

--- find_tests2/commits_not_to_testing.py
+++ find_tests2/commits_not_to_testing.py
@@ -1,6 +1,10 @@
 # python file created
 
 
-import sys
+from pydriller import RepositoryMining, GitRepository  # import necessary libraries
 
-test
+def main():
+    """Driver function. Runs all other necessary functions."""
+    user_repo = input("Enter the link to your chosen GitHub repository: ")
+
+main()  # call the main function; run the program
