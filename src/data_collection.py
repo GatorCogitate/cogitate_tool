5ceff0e92c64d4d8ef2738025fff179ad95bc643
Pedro <carmop@allegheny.edu>
2020-02-25 16:26:47 -0500

    Update description

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,25 +1,5 @@
-""" Collects commit data for a single user. """
+""" Collects commit data for contributors of the master branch of a repo. """
 from pydriller import RepositoryMining
-import pprint
-
-
-# indicate path for repository by pluggin in the URL of repo (must be public)
-repo_path = "https://github.com/GatorCogitate/cogitate_tool"
-
-
-def collect_commits_user_email_key(repo):
-    # TODO: edit the method to take in the URL for the repo
-    """ Creates a dictionary of commit objects for a single user. """
-    # holds email of repo members as keys, contents of commit object as values
-    author_dict = {}
-
-    # loop to turn the each Commits object into the values of the dictionary `author_dict`
-    for commit in RepositoryMining(repo).traverse_commits():
-        if commit.author.email not in author_dict.keys():
-            author_dict[commit.author.email] = [commit]
-        else:
-            author_dict[commit.author.email].append(commit)
-    return author_dict
 
 
 def collect_commits_hash(repo):
@@ -82,6 +62,3 @@ def collect_commits_hash(repo):
         commit_list.append(single_commit_dict)
 
     return commit_list
-
-
-pprint.pprint(collect_commits_hash(repo_path))
