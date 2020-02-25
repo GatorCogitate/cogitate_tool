1320bec73b5053fdf7082f94beb1791a7c06ab5f
Chris Stephenson <stephensonc@allegheny.edu>
2020-02-25 17:10:37 -0500

    Removing __init__.py from root folder fixes pytest

--- conftest.py
+++ conftest.py
@@ -7,6 +7,5 @@ GO_BACK_A_DIRECTORY = "/../"
 GO_INTO_SRC_DIRECTORY = "src"
 
 # set the system path to contain the previous directory
-# PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-PREVIOUS_DIRECTORY = os.path.dirname(__file__)
+PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_SRC_DIRECTORY)
