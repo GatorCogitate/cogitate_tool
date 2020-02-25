65c9cf593230a84f183ef2b9781a55480f324de0
Chris Stephenson <stephensonc@allegheny.edu>
2020-02-25 17:06:35 -0500

    Modify conftest to attempt to fix path

--- conftest.py
+++ conftest.py
@@ -7,5 +7,6 @@ GO_BACK_A_DIRECTORY = "/../"
 GO_INTO_SRC_DIRECTORY = "src"
 
 # set the system path to contain the previous directory
-PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+# PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+PREVIOUS_DIRECTORY = os.path.dirname(__file__)
 sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_SRC_DIRECTORY)
