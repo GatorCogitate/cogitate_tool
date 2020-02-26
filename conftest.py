5b2dce18c7d72b35280bf8e691d4f52b098d9248
Hannah Schultz <schultzh@allegheny.edu>
2020-02-26 13:45:27 -0500

    Shorten line in conftest.py

--- conftest.py
+++ conftest.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-GO_BACK_A_DIRECTORY = "/../"
-GO_INTO_SRC_DIRECTORY = "src"
+GO_BACK_A_DIR = "/../"
+GO_INTO_SRC_DIR = "src"
 
 # set the system path to contain the previous directory
 PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_SRC_DIRECTORY)
+sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIR + GO_INTO_SRC_DIR)
