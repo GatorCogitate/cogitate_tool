802f85110f61c469193ccdfc4e4387e8428ef3d4
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-02-26 15:09:56 -0500

    Add TODO statements for 3/2/2020.

---
+++ src/individual_scoring.py
@@ -0,0 +1,20 @@
+3/3/2020
+
+# TODO Fake data - start with dictionary * (take this from Thomas) - Madelyn 
+# TODO function for commit score - Teona * 
+# TODO function for added lines score - Teona * 
+# TODO function for deleted lines - Wonjoon * 
+# TODO lines modified score - Madelyn * 
+# TODO lines per commit score - Wonjoon * 
+# TODO amount of files modified overall score - Madelyn * 
+# TODO file for printing the individual score - Teona 
+
+# TODO amount of files per commit score 
+# TODO talk about using PyGithub to retrieve issue data 
+# TODO amount of comments in lines of code score - research tools for parsing
+# TODO date distribution score
+# TODO average of all scores
+# TODO average overall score and individual score to produce new individual score
+
+# Use pipeline programming style
+# User inputs the weight for each category for finding total individual score
