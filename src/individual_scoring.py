8eae4dae5903cc8a44a9123f05802127ec15b0a4
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-02-26 18:15:25 -0500

    Add comments to describe TODO statements.

--- src/individual_scoring.py
+++ src/individual_scoring.py
@@ -1,22 +1,23 @@
-3/3/2020
+# For 3/3/2020:
+# TODO Fake data - start with dictionary - Madelyn *
+# TODO function for commit score - Teona *
+# TODO function for added lines score - Teona *
+# TODO function for deleted lines - Wonjoon *
+# TODO lines modified score - Madelyn *
+# TODO lines per commit score - Wonjoon *
+# TODO amount of files modified overall score - Madelyn *
+# TODO file for printing the individual score - Teona *
+# TODO average of all scores - Everyone, once other features are finished
 
-# TODO Fake data - start with dictionary * (take this from Thomas) - Madelyn 
-# TODO function for commit score - Teona * 
-# TODO function for added lines score - Teona * 
-# TODO function for deleted lines - Wonjoon * 
-# TODO lines modified score - Madelyn * 
-# TODO lines per commit score - Wonjoon * 
-# TODO amount of files modified overall score - Madelyn * 
-# TODO file for printing the individual score - Teona * 
-
-# TODO amount of files per commit score 
-# TODO talk about using PyGithub to retrieve issue data 
+# Possible other features or scoring metrics:
+# TODO amount of files per commit score
+# TODO talk about using PyGithub to retrieve issue data
 # TODO amount of comments in lines of code score - research tools for parsing
 # TODO date distribution score
-# TODO average of all scores
 # TODO average overall score and individual score to produce new individual score
 # TODO give notification of duplicate username if there is one, or the possibility
-
-# Use pipeline programming style
 # User inputs the weight for each category for finding total individual score
-# Implement test cases as functions are written 
+
+# Helpful reminders:
+# Use pipeline programming style
+# Implement test cases as functions are written in the test_individual_scoring.py file
