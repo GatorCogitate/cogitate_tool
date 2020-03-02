106bf75e73d24f248bce2293d4a60872be99cf19
Caden Hinckley <chinckley@Cadens-MacBook-Pro.local>
2020-03-02 18:29:48 -0500

    Fixed linting errors in driller.py

--- src/driller.py
+++ src/driller.py
@@ -3,7 +3,7 @@ from pydriller import RepositoryMining
 
 
 def find_repositories(repo):
-    """ Locates a Github repository with the URL provided by the user. """
+    """Locates a Github repository with the URL provided by the user."""
     # ask the user for a URL of a Github repository
     miner = RepositoryMining(path_to_repo=repo)
     return miner
