8dbdf29c65dc4dee5ded16cc097857ce1e0a32fd
Collin McNulty <mcnultyc@alleheny.edu>
2020-03-02 00:16:07 -0500

    adds documentation to test cases in test_issue_retrieval

--- tests/test_issue_retrieval.py
+++ tests/test_issue_retrieval.py
@@ -29,38 +29,52 @@ def test_retrieve_issue_data_retrieves_issues(
     input_token, repository_name, state, contributor_data
 ):
     """Test to ensure all issues are associated with the correct contributor"""
-
+    #PyGithub Object creation.
     ghub = Github(input_token)
     repository = ghub.get_repo(repository_name)
 
     contributor_data = data_miner.retrieve_issue_data(
         repository, state, contributor_data
     )
-
+    #Sets default status to false.
     contributor_found = False
 
+    #Iterates through contributers in a repository.
     for username in contributor_data:
-
+        # Iterates through issues a user has opened. Asserts that it is associating
+        # with the correct user by checking username against username data stored
+        # in the issue.
         for issue_id in contributor_data[username]["issues_opened"]:
             assert repository.get_issue(number=issue_id).pull_request is None
             assert repository.get_issue(number=issue_id).user.login == username
 
+        # Iterates through how many issues a user has commented on, does not count
+        # individual comments.
         for issue_id in contributor_data[username]["issues_commented"]:
             assert repository.get_issue(number=issue_id).pull_request is None
+            # Unnecessary declaration but doesn't hurt anything.
             contributor_found = False
+            # Iterates through comments in a particular issue, until it finds
+            # the username it is searching for, in which case it breaks.
+            # Has the potential to run forever but this is unlikely.
             while contributor_found is False:
                 for comment in repository.get_issue(number=issue_id).get_comments():
                     if comment.user.login == username:
                         contributor_found = True
             assert contributor_found is True
 
+        # Iterates through pull requets opened by a user.
+        # Checks that user hsa opened at least one, and that the username is stored
+        # the same in the pull request data is the user being checked.
         for issue_id in contributor_data[username]["pull_requests_opened"]:
             assert repository.get_issue(number=issue_id).pull_request is not None
             assert repository.get_issue(number=issue_id).user.login == username
 
+        # Checks how many comments a username has on a pull request.
         for issue_id in contributor_data[username]["pull_requests_commented"]:
             assert repository.get_issue(number=issue_id).pull_request is not None
             contributor_found = False
+            # Loops until it finds at least a comment by the username.
             while contributor_found is False:
                 for comment in repository.get_issue(number=issue_id).get_comments():
                     if comment.user.login == username:
