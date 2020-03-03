fa0dbcf6aecefadec199b425f73da8594ada1884
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-03-02 21:14:58 -0500

    Add modified lines fake data.

--- src/Individual_score.py
+++ src/Individual_score.py
@@ -42,16 +42,16 @@ from __future__ import division
 
 # NOTE This is the fake data, it does not have key for the email for now, for tesing purposes
 github_data = {
-    "noorbuchi": {"COMMITS": 28, "ADDED": 349, "REMOVED": 70},
-    "bagashvilit": {"COMMITS": 22, "ADDED": 355, "REMOVED": 56},
-    "Jordan-A": {"COMMITS": 23, "ADDED": 375, "REMOVED": 43},
-    "WonjoonC": {"COMMITS": 27, "ADDED": 365, "REMOVED": 67},
-    "Hannah Schultz": {"COMMITS": 25, "ADDED": 315, "REMOVED": 75},
-    "Alexander_Hamilton": {"COMMITS": 41, "ADDED": 350, "REMOVED": 54},
-    "Karl_Marx": {"COMMITS": 0, "ADDED": 0, "REMOVED": 0},
-    "Julius_Caesar": {"COMMITS": 25, "ADDED": 363, "REMOVED": 35},
-    "Napoleon_Bonaparte": {"COMMITS": 24, "ADDED": 540, "REMOVED": 2},
-    "Alexander_the_Great": {"COMMITS": 42, "ADDED": 355, "REMOVED": 50},
+    "noorbuchi": {"COMMITS": 28, "ADDED": 349, "REMOVED": 70, "MODIFIED": 419},
+    "bagashvilit": {"COMMITS": 22, "ADDED": 355, "REMOVED": 56, "MODIFIED": 411},
+    "Jordan-A": {"COMMITS": 23, "ADDED": 375, "REMOVED": 43, "MODIFIED": 418},
+    "WonjoonC": {"COMMITS": 27, "ADDED": 365, "REMOVED": 67, "MODIFIED": 432},
+    "Hannah Schultz": {"COMMITS": 25, "ADDED": 315, "REMOVED": 75, "MODIFIED": 390},
+    "Alexander_Hamilton": {"COMMITS": 41, "ADDED": 350, "REMOVED": 54, "MODIFIED": 404},
+    "Karl_Marx": {"COMMITS": 0, "ADDED": 0, "REMOVED": 0, "MODIFIED": 0},
+    "Julius_Caesar": {"COMMITS": 25, "ADDED": 363, "REMOVED": 35, "MODIFIED": 398},
+    "Napoleon_Bonaparte": {"COMMITS": 24, "ADDED": 540, "REMOVED": 2, "MODIFIED": 542},
+    "Alexander_the_Great": {"COMMITS": 42, "ADDED": 355, "REMOVED": 50, "MODIFIED": 405},
 }
 
 
@@ -71,7 +71,6 @@ def sum_value(key):
 def individual_commitmnet(username, category):
     """Get and send value for key."""
     return github_data[username][category]
-
 
 def average_score():
     """Calculate the average score using all previously calculated metrics."""
