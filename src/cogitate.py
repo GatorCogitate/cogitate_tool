80049c8b9fbbd88123a8557008d0f66d898d64fb
Xingbang Liu <liux2@allegheny.edu>
2020-03-02 22:30:09 -0500

    url validator added

--- src/cogitate.py
+++ src/cogitate.py
@@ -3,6 +3,7 @@
 from driller import find_repositories
 from pprint import pprint
 import argparse
+import re
 
 
 def main():
@@ -59,8 +60,21 @@ def individual():
     pass
 
 
-def link_validator():
-    pass
+def link_validator(url_str):
+    """Validates if an url is a real link"""
+    url_pattern = re.compile(
+        r"^(?:http|ftp)s?://"  # http:// or https://
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+        r"localhost|"  # localhost...
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+    if re.match(regex, url_str) is not None:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
