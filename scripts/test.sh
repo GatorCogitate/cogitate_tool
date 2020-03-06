f68dd9aa724530f00ea1d5f96936400452e1a48e
clussier <lussierc@allegheny.edu>
2020-03-06 12:51:03 -0500

    Add fixes to project-wide files

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 #!/bin/bash
 
 # Run the test suite so that:
 # --> -x: Stops on first error or failure
 # --> -s: Outputs all diagnostic information
 pipenv run pytest -x -s -v
->>>>>>> master
