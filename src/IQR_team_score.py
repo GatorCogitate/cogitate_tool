a46526c336936206aa96b94cbfc6aae64fc8892d
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-03-05 20:03:20 -0500

    Create IQR function.

--- src/IQR_team_score.py
+++ src/IQR_team_score.py
@@ -1,5 +1,7 @@
 # This import fixes a linting error with old division.
 from __future__ import division
+import numpy as np
+import pandas as pd
 
 # fake data
 github_data = {
@@ -15,3 +17,14 @@ github_data = {
     "Alexander_the_Great": {"COMMITS": 42, "ADDED": 355, "REMOVED": 50, "MODIFIED": 405},
 }
 
+def calculate_iqr(dataset):
+    # sort the dataset in ascending order
+    sorted(dataset)
+    # determine the first and third quartiles
+    q1, q3 = np.percentile(dataset,[25,75])
+    # calculate the interquartile range (q3-q1)
+    iqr = q3 - q1
+    # calculate the lower bound of the IQR
+    lower_bound = q1 -(1.5 * iqr) 
+    # calculate the upper bound of the IQR
+    upper_bound = q3 +(1.5 * iqr) 
