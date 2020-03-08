d3fa4e144207e4a3eaf531aa9a29c0661f0ec862
clussier <lussierc@allegheny.edu>
2020-03-08 19:30:48 -0400

    Improve web interface code formatting

--- src/web_interface.py
+++ src/web_interface.py
@@ -16,42 +16,14 @@ import pandas as pd
 def web_interface():
     """Execute the web interface."""
 
-    ################### Feature 1 ###################
-    # How many commits did an individual make to a GitHub repository?
-
-    ################### Feature 2 ###################
-    # How many lines of code did an individual add, modify, and delete?
-
-    ################### Feature 3 ###################
-    # Why types of files did an individual normally modify in a repository?
-
-    ################### Feature 4 ###################
-    # What is the overall score for an individual’s contribution to a team project?
-
-    ################### Feature 5 ###################
-    # Are there individuals who collaborate together too frequently or not enough?
-
-    ################### Feature 6 ###################
-    # Are there team members who are “code hoarders” or “domain experts”?
-
-    ################### Feature 7 ###################
-    # Are there team members who contribute source code without also adding test cases?
-
-    ################### Feature 8 ###################
-    # Are there team members who break the build or contribute to unusually high code churn?
-
-    ################### Feature 9 ###################
-    # Are there team members who frequently fix the build right before merging a PR to master?
-
-    ################### Feature 10 ###################
-    # Are there team members who are unable to contribute or who seem stuck on finishing a task?
-
-    # Add a selectbox to the sidebar:
+    # Sidebar menu:
     add_selectbox = st.sidebar.selectbox(
         'What feature would you like to view?',
         ('Commits By An Individual', 'Lines of Code Added, Modified, Deleted by an Individual', 'Types of Files Modified by an Individual', 'Overall Contribution Score To Team Project by an Individual', 'Collaboration Tendencies of Individuals', 'Team Members Who Are Code Hoarders', 'Team Members Who Contribute Source Code Without Tests', 'Team Members Who Contribute To High Code Churn', 'Team Members Who Frequently Fix The Build', 'Team Members Who Are Unable To Contribute')
     )
 
+    ################### Feature 1 ###################
+    # How many commits did an individual make to a GitHub repository?
     if add_selectbox == 'Commits By An Individual':
         st.title("Commit Information")  # dispaly relevant title for dataframe
 
@@ -76,8 +48,40 @@ def web_interface():
         )  # allow users to display specific contributor information on dataframe graph
 
         st.line_chart(df[columns])  # display dataframe/graph that vizualizes commit info
+
+    ################### Feature 2 ###################
+    # How many lines of code did an individual add, modify, and delete?
+    elif add_selectbox == 'Lines of Code Added, Modified, Deleted by an Individual':
+        st.title("Lines of Code Added, Modified, Deleted by an Individual")  # dispaly relevant title for dataframe
+
+    ################### Feature 3 ###################
+    # Why types of files did an individual normally modify in a repository?
+
+    ################### Feature 4 ###################
+    # What is the overall score for an individual’s contribution to a team project?
+
+    ################### Feature 5 ###################
+    # Are there individuals who collaborate together too frequently or not enough?
+
+    ################### Feature 6 ###################
+    # Are there team members who are “code hoarders” or “domain experts”?
+
+    ################### Feature 7 ###################
+    # Are there team members who contribute source code without also adding test cases?
+
+    ################### Feature 8 ###################
+    # Are there team members who break the build or contribute to unusually high code churn?
+
+    ################### Feature 9 ###################
+    # Are there team members who frequently fix the build right before merging a PR to master?
+
+    ################### Feature 10 ###################
+    # Are there team members who are unable to contribute or who seem stuck on finishing a task?
     else:
         pass
+
+
+
 
 
 
