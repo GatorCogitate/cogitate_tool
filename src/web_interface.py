8de69afdce83d5b9e60083cde1228e6d915ff8c6
clussier <lussierc@allegheny.edu>
2020-03-08 19:13:59 -0400

    Add web_interface.py file

---
+++ src/web_interface.py
@@ -0,0 +1,59 @@
+"""Web Interface for interacting with Github repository info."""
+
+# from data_collection import collect_commits
+import argparse
+
+# from pprint import pprint
+
+# from driller import find_repositories
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+def web_interface():
+    """Execute the web interface."""
+
+    ################### Feature 1 ###################
+    
+    ################### Feature 2 ###################
+    ################### Feature 3 ###################
+    ################### Feature 4 ###################
+    ################### Feature 5 ###################
+    ################### Feature 6 ###################
+    ################### Feature 7 ###################
+    ################### Feature 8 ###################
+    ################### Feature 9 ###################
+    ################### Feature 10 ###################
+    ################### Feature 11 ###################
+    ################### Feature 12 ###################
+
+    st.title("Commit Information")  # dispaly relevant title for dataframe
+
+    df = pd.DataFrame({
+      'date': ['10/1/2019','10/2/2019', '10/3/2019', '10/4/2019'],
+      'Christian Lussier': [8, 5, 9, 3],
+      'Cory Wiard': [5, 9, 3, 5],
+      'Devin Spitalny': [2, 5, 7, 3],
+      'Devin Ho': [8, 9, 2, 1],
+      'Jordan Wilson': [5, 9, 3, 8],
+      'Danny Reid': [5, 4, 3, 5],
+      'Anthony Baldeosingh': [1, 2, 1, 2],
+      'Xingbang Liu': [6, 9, 4, 7]
+    })  # create dataframe with sample dates and contributor commit numbers
+
+    df = df.rename(columns={'date':'index'}).set_index('index')  # set date as index
+
+    df  # display chart of sample commits
+
+    columns = st.multiselect(
+        label="Enter the names of specific contributors below:", options=df.columns
+    )  # allow users to display specific contributor information on dataframe graph
+
+    st.line_chart(df[columns])  # display dataframe/graph that vizualizes commit info
+
+
+
+web_interface()
