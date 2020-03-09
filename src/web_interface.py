14e1eae78fce851dd557173cf16efe0af59d397e
Tyler Pham <phamt@allegheny.edu>
2020-03-08 22:46:50 -0400

    Add title for feature 3.

--- src/web_interface.py
+++ src/web_interface.py
@@ -53,31 +53,10 @@ def web_interface():
     # How many lines of code did an individual add, modify, and delete?
     elif add_selectbox == 'Lines of Code Added, Modified, Deleted by an Individual':
         st.title("Lines of Code Added, Modified, Deleted by an Individual")  # dispaly relevant title for dataframe
-        df = pd.DataFrame({
-          'type': ['Lines Added','Lines Modified', 'Lines Deleted'],
-          'Christian Lussier': [8, 5, 9],
-          'Cory Wiard': [5, 9, 3],
-          'Devin Spitalny': [2, 5, 7],
-          'Devin Ho': [8, 9, 2],
-          'Jordan Wilson': [5, 9, 3],
-          'Danny Reid': [5, 4, 3],
-          'Anthony Baldeosingh': [1, 2, 1],
-          'Xingbang Liu': [6, 9, 4]
-        })  # create dataframe with sample dates and contributor commit numbers
-
-        df = df.rename(columns={'type':'index'}).set_index('index')  # set date as index
-
-        df  # display chart of sample commits
-
-        columns = st.multiselect(
-            label="Enter the names of specific contributors below:", options=df.columns
-        )  # allow users to display specific contributor information on dataframe graph
-
-
-        st.bar_chart(df[columns])  # display dataframe/graph that vizualizes commit info
 
     ################### Feature 3 ###################
     # Why types of files did an individual normally modify in a repository?
+    elif add_selectbox == 'Types of Files Modified by an Individual':
 
     ################### Feature 4 ###################
     # What is the overall score for an individual’s contribution to a team project?
@@ -93,23 +72,6 @@ def web_interface():
     # Are there team members who contribute source code without also adding test cases?
     elif add_selectbox == 'Team Members Who Contribute Source Code Without Tests':
         st.title("Team Members Who Contribute Source Code Without Tests")
-        df = pd.DataFrame({
-          'date': ['10/1/2019','10/2/2019', '10/3/2019', '10/4/2019'],
-          'Christian Lussier': [8, 5, 9, 3],
-          'Cory Wiard': [5, 9, 3, 5],
-          'Devin Spitalny': [2, 5, 7, 3],
-          'Devin Ho': [8, 9, 2, 1],
-          'Jordan Wilson': [5, 9, 3, 8],
-          'Danny Reid': [5, 4, 3, 5],
-          'Anthony Baldeosingh': [1, 2, 1, 2],
-          'Xingbang Liu': [6, 9, 4, 7]
-        })
-
-        df = df.rename(columns={'date':'index'}).set_index('index')  # set date as index
-
-        df
-
-        
     ################### Feature 8 ###################
     # Are there team members who break the build or contribute to unusually high code churn?
 
