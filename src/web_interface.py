802882a350b4c621ee73b25d835649cb34548a6a
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-03-09 13:37:17 -0400

    Add code back into feature 7

--- src/web_interface.py
+++ src/web_interface.py
@@ -79,7 +79,7 @@ def web_interface():
     # Are there individuals who collaborate together too frequently or not enough?
     elif add_selectbox == 'Collaboration Tendencies of Individuals':
         st.title("Collaboration Tendencies of Individuals")
-        
+
         df = pd.DataFrame({
           'type': ['Individuals overall contribution'],
           'Christian Lussier': [28, 255, 75],
@@ -134,6 +134,28 @@ def web_interface():
     # Are there team members who contribute source code without also adding test cases?
     elif add_selectbox == 'Team Members Who Contribute Source Code Without Tests':
         st.title("Team Members Who Contribute Source Code Without Tests")
+        df = pd.DataFrame({
+          'date': ['10/1/2019','10/2/2019', '10/3/2019', '10/4/2019'],
+          'Christian Lussier': [8, 5, 9, 3],
+          'Cory Wiard': [5, 9, 3, 5],
+          'Devin Spitalny': [2, 5, 7, 3],
+          'Devin Ho': [8, 9, 2, 1],
+          'Jordan Wilson': [5, 9, 3, 8],
+          'Danny Reid': [5, 4, 3, 5],
+          'Anthony Baldeosingh': [1, 2, 1, 2],
+          'Xingbang Liu': [6, 9, 4, 7]
+        })  # create dataframe with sample dates and contributor commit numbers
+
+        df = df.rename(columns={'date':'index'}).set_index('index')  # set date as index
+
+        df  # display chart of sample commits
+
+        columns = st.multiselect(
+            label="Enter the names of specific contributors below:", options=df.columns
+        )  # allow users to display specific contributor information on dataframe graph
+
+        st.line_chart(df[columns])  # display dataframe/graph that vizualizes commit info
+
     ################### Feature 8 ###################
     # Are there team members who break the build or contribute to unusually high code churn?
     elif add_selectbox == 'Team Members Who Contribute To High Code Churn':
