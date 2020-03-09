27e638bd30c670753b0463b417c5e3e085169faf
bagashvilit <bagashvilit@allegheny.edu>
2020-03-09 18:40:43 -0400

    Make the function return type

--- src/Individual_score.py
+++ src/Individual_score.py
@@ -6,6 +6,8 @@ based on a data set gathered previously in a different module.
 
 # This import fixes a linting error with old division.
 from __future__ import division
+from collections import defaultdict
+import pandas as pd
 import data_collection
 
 # Helpful reminders:
@@ -21,38 +23,24 @@ import data_collection
 # NOTE This is the fake data, it does not have key for the email for now, for tesing purposes
 github_data = {
     "noorbuchi": {"email": "email", "COMMITS": 28, "ADDED": 349, "REMOVED": 70},
-    "bagashvilit": {"email": "email", "COMMITS": 22, "ADDED": 355, "REMOVED": 56},
-    "Jordan-A": {"email": "email", "COMMITS": 23, "ADDED": 375, "REMOVED": 43},
-    "WonjoonC": {"email": "email", "COMMITS": 27, "ADDED": 365, "REMOVED": 67},
-    "Hannah Schultz": {"email": "email", "COMMITS": 25, "ADDED": 315, "REMOVED": 75},
-    "Alexander_Hamilton": {
-        "email": "email",
-        "COMMITS": 41,
-        "ADDED": 350,
-        "REMOVED": 54,
-    },
-    "Karl_Marx": {"email": "email", "COMMITS": 0, "ADDED": 0, "REMOVED": 0},
-    "Julius_Caesar": {"email": "email", "COMMITS": 25, "ADDED": 363, "REMOVED": 35},
-    "Napoleon_Bonaparte": {"email": "email", "COMMITS": 24, "ADDED": 540, "REMOVED": 2},
-    "Alexander_the_Great": {
-        "email": "email",
-        "COMMITS": 42,
-        "ADDED": 355,
-        "REMOVED": 50,
-    },
+    "bagashvilit": {"email": "email","COMMITS": 22, "ADDED": 355, "REMOVED": 56},
+    "Jordan-A": {"email": "email","COMMITS": 23, "ADDED": 375, "REMOVED": 43},
+    "WonjoonC": {"email": "email","COMMITS": 27, "ADDED": 365, "REMOVED": 67},
+    "Hannah Schultz": {"email": "email","COMMITS": 25, "ADDED": 315, "REMOVED": 75},
+    "Alexander_Hamilton": {"email": "email","COMMITS": 41, "ADDED": 350, "REMOVED": 54},
+    "Karl_Marx": {"email": "email","COMMITS": 0, "ADDED": 0, "REMOVED": 0},
+    "Julius_Caesar": {"email": "email","COMMITS": 25, "ADDED": 363, "REMOVED": 35},
+    "Napoleon_Bonaparte": {"email": "email","COMMITS": 24, "ADDED": 540, "REMOVED": 2},
+    "Alexander_the_Great": {"email": "email","COMMITS": 42, "ADDED": 355, "REMOVED": 50},
 }
 
 # NOTE: The following code block still needs to be fixed in terms of variable
 # names and docstrings.
 
-# github_data = data_collection.calculate_individual_metrics("individual_metrics_testfile")
+#github_data = data_collection.calculate_individual_metrics("individual_metrics_testfile")
 # pylint: disable=round-builtin
-data_collection.add_raw_data_to_json(
-    "/home/teona/Documents/CS203/project/cogitate_tool", "individual_score_test"
-)
-github_data = data_collection.calculate_individual_metrics("individual_score_test")
-
-
+# data_collection.add_raw_data_to_json("/home/teona/Documents/CS203/project/cogitate_tool", "individual_score_test")
+# github_data = data_collection.calculate_individual_metrics("individual_score_test")
 def percentage_contribution(individual, overal_branch):
     """Calculate the individual contribution percentage."""
     return round(individual * 100 / overal_branch)
@@ -63,25 +51,27 @@ def sum_value(key):
     return sum(d[key] for d in github_data.values() if d)
 
 
-# Print usename and percentage of their contribution for each category
-def print_data():
-    """Print out github_data scores."""
+def contribution():
+    contributor_data = {}
+    contributor_data = defaultdict(dict)
     for username, data in github_data.items():
-        print("\n", username)
-        for category, value in data.items():
+        for category,value in data.items():
             if isinstance(value, int):
-                print(
-                    category, percentage_contribution(value, sum_value(category)), "%",
-                )
+                contributor_data[username][category] = percentage_contribution(
+                    value, sum_value(category))
+    return contributor_data
+
+
 
 
 if __name__ == "__main__":
 
     # for username in github_data:
     #     print (get_weighted(username))
-    # print(data_collection.calculate_individual_metrics("individual_metrics_testfile"))
-    print_data()
-
+    #print(data_collection.calculate_individual_metrics("individual_metrics_testfile"))
+    #print_data()
+    print(contribution())
+    print(pd.DataFrame.from_dict(contribution()))
     # def individual_commitmnet(username, category):
     #     """Get and send value for key."""
     #     return github_data.get(username).get(category)
@@ -96,3 +86,18 @@ if __name__ == "__main__":
     #
     #     weighted={User: {k:(round(user[k]*weights[k])) for k in user.keys()}}
     #     return weighted
+
+    #Print usename and percentage of their contribution for each category
+    # def print_data():
+    #     """Print out github_data scores."""
+    #     for username, data in github_data.items():
+    #         print("\n", username)
+    #         for category,value in data.items():
+    #             if isinstance(value, int):
+    #                 print(
+    #                     category,
+    #                     percentage_contribution(
+    #                         value, sum_value(category)
+    #                         ),
+    #                         "%",
+    #                         )
