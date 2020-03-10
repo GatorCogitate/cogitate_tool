74a19c359c079c0bbe64195205ae09f3709c844c
Xingbang Liu <liux2@allegheny.edu>
2020-03-09 20:12:49 -0400

    Helping message updated

--- src/cogitate.py
+++ src/cogitate.py
@@ -21,29 +21,29 @@ def main():
     contributor_data = data_collection.retrieve_issue_data(
         repository, args["state"], contributor_data
     )
-    link_str = str(args["link"])
-    if link_validator(link_str) is True:
-        data_collection.collect_commits_hash(args["link"])
-    else:
-        print("The link you have entered is invalid.")
-    # gives the user the option to use the web interface
-    web = True
-    # while loop to ensure user input is "y" or "n"
-    while web is True:
-        # pylint: disable=input-builtin
-        visit_web = input("Would you like to view the data on the web?(y/n)")
-        if visit_web == "y":
-            # print(web_interface.web_interface())
-            print("link")
-            web = False
-        elif visit_web == "n":
-            data_collection.print_file("contributor_data", ["repo"])
-            # exits loop when user chooses not to use the web interface
-            web = False
-        else:
-            print("Please enter (y/n).")
-            # loop is repeated when output is not "y" or "n"
-    print("Thank you for using GatorCogitate. Have a great day!")
+    # link_str = str(args["link"])
+    # if link_validator(link_str) is True:
+    #     data_collection.collect_commits_hash(args["link"])
+    # else:
+    #     print("The link you have entered is invalid.")
+    # # gives the user the option to use the web interface
+    # web = True
+    # # while loop to ensure user input is "y" or "n"
+    # while web is True:
+    #     # pylint: disable=input-builtin
+    #     visit_web = input("Would you like to view the data on the web?(y/n)")
+    #     if visit_web == "y":
+    #         # print(web_interface.web_interface())
+    #         print("link")
+    #         web = False
+    #     elif visit_web == "n":
+    #         data_collection.print_file("contributor_data", ["repo"])
+    #         # exits loop when user chooses not to use the web interface
+    #         web = False
+    #     else:
+    #         print("Please enter (y/n).")
+    #         # loop is repeated when output is not "y" or "n"
+    # print("Thank you for using GatorCogitate. Have a great day!")
     # end of the CLI main method
 
 
@@ -57,16 +57,24 @@ def retrieve_arguments():
         "-l",
         "--link",
         type=link_validator,
-        help="Cogitate a repo by the url of the repo",
+        help="Cogitate a repo by the url of the repo, require full link of the repo",
     )
     a_parse.add_argument(
         "-t", "--token", required=True, type=str, help="Github User Token"
     )
     a_parse.add_argument(
-        "-r", "--repo", required=True, type=str, help="User's Repository"
+        "-r",
+        "--repo",
+        required=True,
+        type=str,
+        help="User's Repository name, start with root dirctory (user or organization name)",
     )
     a_parse.add_argument(
-        "-s", "--state", required=False, type=str, help="State of the Issue"
+        "-s",
+        "--state",
+        required=False,
+        type=str,
+        help="State of the Issue, open or closed",
     )
 
     args = vars(a_parse.parse_args())
@@ -86,8 +94,10 @@ def individual():
 
 def link_validator(url_str):
     """Take a string and checks if it is a valid URL."""
-    # returns a boolean value
-    return validators.url(url_str)
+    # url() returns a boolean value
+    if not validators.url(url_str):
+        raise argparse.ArgumentTypeError("%s is not an URL" % url_str)
+    return url_str
 
 
 if __name__ == "__main__":
