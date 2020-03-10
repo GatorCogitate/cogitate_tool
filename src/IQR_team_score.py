440a5a636281f8a55e98c6b97bc53657bb146284
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-03-09 23:02:55 -0400

    Remove fake data.

--- src/IQR_team_score.py
+++ src/IQR_team_score.py
@@ -6,20 +6,62 @@ import numpy as np
 import data_collection
 
 
-# github_data = data_collection.calculate_individual_metrics()
+github_data = data_collection.calculate_individual_metrics()
 
-github_data = {
-    "noorbuchi": {"COMMITS": 28, "ADDED": 349, "REMOVED": 70},
-    "bagashvilit": {"COMMITS": 22, "ADDED": 355, "REMOVED": 56},
-    "Jordan-A": {"COMMITS": 23, "ADDED": 375, "REMOVED": 43},
-    "WonjoonC": {"COMMITS": 27, "ADDED": 365, "REMOVED": 67},
-    "Hannah Schultz": {"COMMITS": 25, "ADDED": 315, "REMOVED": 75},
-    "Alexander_Hamilton": {"COMMITS": 41, "ADDED": 350, "REMOVED": 54},
-    "Karl_Marx": {"COMMITS": 0, "ADDED": 0, "REMOVED": 0},
-    "Julius_Caesar": {"COMMITS": 25, "ADDED": 363, "REMOVED": 35},
-    "Napoleon_Bonaparte": {"COMMITS": 24, "ADDED": 540, "REMOVED": 2},
-    "Alexander_the_Great": {"COMMITS": 42, "ADDED": 355, "REMOVED": 50},
-}
+# github_data = {
+#     "noorbuchi": {"COMMITS": 28, "ADDED": 349, "REMOVED": 70},
+#     "bagashvilit": {"COMMITS": 22, "ADDED": 355, "REMOVED": 56},
+#     "Jordan-A": {"COMMITS": 23, "ADDED": 375, "REMOVED": 43},
+#     "WonjoonC": {"COMMITS": 27, "ADDED": 365, "REMOVED": 67},
+#     "Hannah Schultz": {"COMMITS": 25, "ADDED": 315, "REMOVED": 75},
+#     "Alexander_Hamilton": {"COMMITS": 41, "ADDED": 350, "REMOVED": 54},
+#     "Karl_Marx": {"COMMITS": 0, "ADDED": 0, "REMOVED": 0},
+#     "Julius_Caesar": {"COMMITS": 25, "ADDED": 363, "REMOVED": 35},
+#     "Napoleon_Bonaparte": {"COMMITS": 24, "ADDED": 540, "REMOVED": 2},
+#     "Alexander_the_Great": {"COMMITS": 42, "ADDED": 355, "REMOVED": 50},
+# }
+
+
+def calculate_iqr_score(data_list):
+    """Calculate a team score for a data set according to outliers calculated with the interquartile range."""
+    below_amount = 0
+    above_amount = 0
+    within_amount = 0
+    dataset = data_list
+    # calculate the size of the dataset
+    size = len(dataset)
+    # sort the dataset in ascending order
+    sorted(dataset)
+    # determine the first and third quartiles
+    q1, q3 = np.percentile(dataset, [25, 75])
+    # calculate the interquartile range (q3-q1)
+    iqr = q3 - q1
+    # calculate the lower bound of the IQR
+    lower_bound = q1 - (1.5 * iqr)
+    # calculate the upper bound of the IQR
+    upper_bound = q3 + (1.5 * iqr)
+    # for any value above, below or within the IQR add to the total
+    for d in dataset:
+        if d < lower_bound:
+            below_amount = below_amount + 1
+        if d > upper_bound:
+            above_amount = above_amount + 1
+        if lower_bound <= d <= upper_bound:
+            within_amount = within_amount + 1
+    # create factions of above, below, and within IQR amounts (divide by size) and round
+    below_fraction = round(below_amount / size, 2)
+    above_fraction = round(above_amount / size, 2)
+    within_fraction = round(within_amount / size, 2)
+
+    # print(below_amount, above_amount, within_amount)
+    # calculate all areas with their weight measurement
+    weighted_below = round(0.20 * below_fraction, 2)
+    weighted_above = round(0.20 * above_fraction, 2)
+    weighted_within = round(0.60 * within_fraction, 2)
+    # add weighted scores together to calcuate overall team score as percentage
+    category_score = (weighted_below + weighted_above + weighted_within) * 100
+
+    return category_score
 
 
 def calculate_team_score(dictionary):
@@ -77,48 +119,6 @@ def calculate_team_score(dictionary):
     # amount_of_lists = amount_of_lists + 1
     # team_score = total_score / amount_of_lists
     # print(generic_list)
-
-
-def calculate_iqr_score(data_list):
-    """Calculate a team score for a data set according to outliers calculated with the interquartile range."""
-    below_amount = 0
-    above_amount = 0
-    within_amount = 0
-    dataset = data_list
-    # calculate the size of the dataset
-    size = len(dataset)
-    # sort the dataset in ascending order
-    sorted(dataset)
-    # determine the first and third quartiles
-    q1, q3 = np.percentile(dataset, [25, 75])
-    # calculate the interquartile range (q3-q1)
-    iqr = q3 - q1
-    # calculate the lower bound of the IQR
-    lower_bound = q1 - (1.5 * iqr)
-    # calculate the upper bound of the IQR
-    upper_bound = q3 + (1.5 * iqr)
-    # for any value above, below or within the IQR add to the total
-    for d in dataset:
-        if d < lower_bound:
-            below_amount = below_amount + 1
-        if d > upper_bound:
-            above_amount = above_amount + 1
-        if lower_bound <= d <= upper_bound:
-            within_amount = within_amount + 1
-    # create factions of above, below, and within IQR amounts (divide by size) and round
-    below_fraction = round(below_amount / size, 2)
-    above_fraction = round(above_amount / size, 2)
-    within_fraction = round(within_amount / size, 2)
-
-    # print(below_amount, above_amount, within_amount)
-    # calculate all areas with their weight measurement
-    weighted_below = round(0.20 * below_fraction, 2)
-    weighted_above = round(0.20 * above_fraction, 2)
-    weighted_within = round(0.60 * within_fraction, 2)
-    # add weighted scores together to calcuate overall team score as percentage
-    category_score = (weighted_below + weighted_above + weighted_within) * 100
-
-    return category_score
 
 
 if __name__ == "__main__":
