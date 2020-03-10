3eda3908bdb486679c7a795f8093ccc7939b8578
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-03-09 20:11:27 -0400

    Add fake data for testing purposes.

--- src/IQR_team_score.py
+++ src/IQR_team_score.py
@@ -9,16 +9,42 @@ commit_data = []
 added_data = []
 removed_data = []
 modified_data = []
-github_data = {}
+# github_data = {}
+
+github_data = {
+    "noorbuchi": {"email": "email", "COMMITS": 28, "ADDED": 349, "REMOVED": 70},
+    "bagashvilit": {"email": "email","COMMITS": 22, "ADDED": 355, "REMOVED": 56},
+    "Jordan-A": {"email": "email","COMMITS": 23, "ADDED": 375, "REMOVED": 43},
+    "WonjoonC": {"email": "email","COMMITS": 27, "ADDED": 365, "REMOVED": 67},
+    "Hannah Schultz": {"email": "email","COMMITS": 25, "ADDED": 315, "REMOVED": 75},
+    "Alexander_Hamilton": {"email": "email","COMMITS": 41, "ADDED": 350, "REMOVED": 54},
+    "Karl_Marx": {"email": "email","COMMITS": 0, "ADDED": 0, "REMOVED": 0},
+    "Julius_Caesar": {"email": "email","COMMITS": 25, "ADDED": 363, "REMOVED": 35},
+    "Napoleon_Bonaparte": {"email": "email","COMMITS": 24, "ADDED": 540, "REMOVED": 2},
+    "Alexander_the_Great": {"email": "email","COMMITS": 42, "ADDED": 355, "REMOVED": 50},
+}
+# data_collection.add_raw_data_to_json(
+#     "/home/maddykapfhammer/Documents/Allegheny/2020/Spring/CS203/cogitate_tool", "team_score_test"
+# )
+# github_data = data_collection.calculate_individual_metrics("team_score_test")
+
+def determine_datasets(dictionary):
+    """Determine the datasets of the information in the dictionary and add them to the corresponding list."""
+    # iterate through nested dictionary
+    for username, data in github_data.items():
+        # for each key in the dictionary add its values to a list
+        for key in data:
+            if key == "COMMITS":
+                commit_data.append(data[key])
+            if key == "ADDED":
+                added_data.append(data[key])
+            if key == "REMOVED":
+                removed_data.append(data[key])
+            # if key == "MODIFIED":
+            #     modified_data.append(data[key])
 
 
-data_collection.add_raw_data_to_json(
-    "/home/maddykapfhammer/Documents/Allegheny/2020/Spring/CS203/cogitate_tool", "team_score_test"
-)
-github_data = data_collection.calculate_individual_metrics("team_score_test")
-
-
-def calculate_iqr_score(data_list, below_weight, above_weight, within_weight):
+def calculate_iqr_score(data_list):
     """Calculate a team score for a data set according to outliers calculated with the interquartile range."""
     below_amount = 0
     above_amount = 0
@@ -51,30 +77,13 @@ def calculate_iqr_score(data_list, below_weight, above_weight, within_weight):
 
     # print(below_amount, above_amount, within_amount)
     # calculate all areas with their weight measurement
-    weighted_below = round(below_weight * below_fraction, 2)
-    weighted_above = round(above_weight * above_fraction, 2)
-    weighted_within = round(within_weight * within_fraction, 2)
+    weighted_below = round(0.20 * below_fraction, 2)
+    weighted_above = round(0.20 * above_fraction, 2)
+    weighted_within = round(0.60 * within_fraction, 2)
     # add weighted scores together to calcuate overall team score as percentage
     team_score = (weighted_below + weighted_above + weighted_within) * 100
 
     return team_score
-
-
-def determine_datasets(dictionary):
-    """Determine the datasets of the information in the dictionary and add them to the corresponding list."""
-    # iterate through nested dictionary
-    for username, data in github_data.items():
-        # for each key in the dictionary add its values to a list
-        for key in data:
-            if key == "COMMITS":
-                commit_data.append(data[key])
-            if key == "ADDED":
-                added_data.append(data[key])
-            if key == "REMOVED":
-                removed_data.append(data[key])
-            if key == "MODIFIED":
-                modified_data.append(data[key])
-
 
 
 def calculate_average(dictionary):
@@ -82,20 +91,17 @@ def calculate_average(dictionary):
     commits_score = 0
     added_score = 0
     removed_score = 0
-    modified_score = 0
+    # modified_score = 0
     average_score = 0
-    below_weight = 0.05
-    above_weight = 0.2
-    within_weight = 0.75
     # import lists from calculate_datasets function
     determine_datasets(dictionary)
     # calculate the team score for each list of data
-    commits_score = calculate_iqr_score(commit_data, below_weight, above_weight, within_weight)
-    added_score = calculate_iqr_score(added_data, below_weight, above_weight, within_weight)
-    removed_score = calculate_iqr_score(removed_data, below_weight, above_weight, within_weight)
-    modified_score = calculate_iqr_score(modified_data, below_weight, above_weight, within_weight)
+    commits_score = calculate_iqr_score(commit_data)
+    added_score = calculate_iqr_score(added_data)
+    removed_score = calculate_iqr_score(removed_data)
+    # modified_score = calculate_iqr_score(modified_data)
     # find the average team score for all of the categories
-    average_score = (commits_score + added_score + removed_score + modified_score) / 4
+    average_score = (commits_score + added_score + removed_score) / 4
 
     return average_score
 
