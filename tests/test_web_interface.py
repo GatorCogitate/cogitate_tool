4df56d1752bfe7f2a933737348894a1def622c0c
clussier <lussierc@allegheny.edu>
2020-03-10 15:38:00 -0400

    Add test_web_interface.py

---
+++ tests/test_web_interface.py
@@ -0,0 +1,7 @@
+"""
+Test module to determine the correctness of the web_interface.py file.
+"""
+import pytest
+from src import data_collection
+from src import json_handler
+from src import web_interface
