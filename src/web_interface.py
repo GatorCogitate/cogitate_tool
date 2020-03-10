f2b5dc8312b8eddc3df0b5096106dc2c55e9be94
Jordan Wilson <wilsonj3@allegheny.edu>
2020-03-10 13:26:35 -0400

    Add code to feature 4

--- src/web_interface.py
+++ src/web_interface.py
@@ -85,20 +85,44 @@ def web_interface():
     # What is the overall score for an individual’s contribution to a team project?
     elif add_selectbox == 'An individuals overall contribution to a team or project':
         st.title("An individuals overall contribution to a team or project")
+
+        df = pd.DataFrame({
+            'date': ['10/1/2019','10/2/2019', '10/3/2019', '10/4/2019'],
+            'Christian Lussier': [7, 5, 5, 3],
+            'Cory Wiard': [2, 1, 3, 5],
+            'Devin Spitalny': [8, 1, 0, 7],
+            'Devin Ho': [9, 9, 2, 5],
+            'Jordan Wilson': [5, 9, 3, 8],
+            'Danny Reid': [1, 2, 3, 1],
+            'Anthony Baldeosingh': [7, 4, 3, 4],
+            'Xingbang Liu': [6, 6, 8, 2]
+        })  # create dataframe with sample data for files
+
+        df = df.rename(columns={'date':'index'}).set_index('index')  # set date as index
+
+        df  # display chart of sample commits
+
+        columns = st.multiselect(
+            label="Enter the names of specific contributors below:", options=df.columns
+        )  # allow users to display specific contributor information on dataframe graph
+
+        st.line_chart(df[columns])  # display dataframe/graph that vizualizes commit info
+
     ################### Feature 5 ###################
     # Are there individuals who collaborate together too frequently or not enough?
     if add_selectbox == 'Collaboration Tendencies of Individuals':
-        st.title("Collaboration Tendencies of Individuals")  # dispaly relevant
+        st.title("Collaboration Tendencies of Individuals")  # dispaly relevant title for dataframe
+
         df = pd.DataFrame({
-          'date': ['1/1/2020','1/2/2020', '1/3/2020', '10/4/2020'],
-          'Christian Lussier': [80, 5, 9, 3],
-          'Cory Wiard': [5, 90, 3, 5],
-          'Devin Spitalny': [23, 58, 70, 3],
-          'Devin Ho': [8, 62, 2, 11],
-          'Jordan Wilson': [5, 5, 3, 8],
-          'Danny Reid': [50, 41, 311, 5],
-          'Anthony Baldeosingh': [10, 200, 1, 22],
-          'Xingbang Liu': [60, 19, 14, 17]
+          'type': ['Individuals overall contribution'],
+          'Christian Lussier': [28, 255, 75],
+          'Cory Wiard': [6, 349, 50],
+          'Devin Spitalny': [22, 15, 48],
+          'Devin Ho': [8, 128, 2],
+          'Jordan Wilson': [10, 8, 28],
+          'Danny Reid': [25, 5, 15],
+          'Anthony Baldeosingh': [1, 1, 1],
+          'Xingbang Liu': [6, 100, 129]
         })  # create dataframe with sample dates and contributor commit numbers
 
         df = df.rename(columns={'date':'index'}).set_index('index')  # set date as index
