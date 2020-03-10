f8ece675d6a9e81412879e6737ede63b68a35cfb
bagashvilit <bagashvilit@allegheny.edu>
2020-03-09 23:05:52 -0400

    Fix docstrings and add documentation

--- src/Individual_score.py
+++ src/Individual_score.py
@@ -1,6 +1,6 @@
 """
-This module's purpose is to calculate the individual scoring of software.
-developers in a Github repository. It will calculate a developer's score
+This module's purpose is to calculate the individual contribution of software.
+developers in a Github repository. It will calculate a developer's contribution
 based on a data set gathered previously in a different module.
 """
 
@@ -13,42 +13,39 @@ import data_collection
 
 # pylint: disable=round-builtin
 def percent_calculator(individual, overal_branch):
-    """Calculate the individual contribution percentage."""
+    """Function to calculate the percentage."""
     return round(individual * 100 / overal_branch)
 
 
 def sum_metrics_int(key, dictionary):
-    """Sum up all the values in metrics per key."""
+    """Sum up all the int type values in metrics per key."""
     return sum(d[key] for d in dictionary.values())
 
 
 def sum_metrics_list(key, dictionary):
-    """Sum up all the values in metrics per key."""
+    """Sum up all the list type values in metrics per key."""
     return sum(len(d[key]) for d in dictionary.values())
 
 
 def individual_contribution(dictionary):
     """Calculate the percentage of indivudual contribution."""
     contributor_data = {}
+    #use default dictionary to provide default value for a nonexistent key
     contributor_data = defaultdict(dict)
     for username, data in dictionary.items():
         for metrics, value in data.items():
+            #if data type is int use the appropriate function to sum up the values
             if isinstance(value, int):
-                contributor_data[username][metrics] = (
-                    percent_calculator(value, sum_metrics_int(metrics, dictionary)),
-                    "%",
-                )
+                contributor_data[username][metrics] = percent_calculator(
+                    value, sum_metrics_int(metrics, dictionary)), "%"
+            #if data type is list use the appropriate function to sum up the values
             if isinstance(value, list):
-                contributor_data[username][metrics] = (
-                    percent_calculator(
-                        len(value), sum_metrics_list(metrics, dictionary)
-                    ),
-                    "%",
-                    value,
-                )
+                contributor_data[username][metrics] = percent_calculator(
+                    len(value), sum_metrics_list(metrics, dictionary)), "%", value
     return contributor_data
 
 
+#This part of the program is for demonstration and debugging puproses
 if __name__ == "__main__":
 
     DATA = data_collection.calculate_individual_metrics()
@@ -58,6 +55,6 @@ if __name__ == "__main__":
         REPO_PATH = input("Enter the path to the repo : ")
         # This call will use default options for file and overwrite condition
         data_collection.collect_and_add_raw_data_to_json(REPO_PATH)
-    print("Adding processed data to selected json file...")
 
+    #display the dictionary of individual contributions
     print(pd.DataFrame.from_dict(individual_contribution(DATA)).T)
