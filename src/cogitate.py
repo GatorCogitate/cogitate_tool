f21ca416f063c5f8c090c800a87e67f23ac1612c
StringerJ <stringerj@allegheny.edu>
2020-03-09 22:57:17 -0400

    added if else statement for link argument, added default value to 'state' argument and 'web' argument, changed 'web' argument to required=False

--- src/cogitate.py
+++ src/cogitate.py
@@ -2,48 +2,28 @@
 
 import argparse
 import validators
-
 # from web_interface import web_interface
 import data_collection
 
 
 def main(args):
     """Execute the CLI."""
+
     # Currently only validates the PyGithub repository
     repository = data_collection.authenticate_repository(args["token"], args["repo"])
-
-    # Temporary structure given issue retrieval is the only function
-    contributor_data = data_collection.initialize_contributor_data(
-        "contributor_data_template"
-    )
-    contributor_data = data_collection.retrieve_issue_data(
-        repository, args["state"], contributor_data
-    )
-    # link_str = str(args["link"])
-    # if link_validator(link_str) is True:
-    #     data_collection.collect_commits_hash(args["link"])
-    # else:
-    #     print("The link you have entered is invalid.")
-
-    # # gives the user the option to use the web interface
-    # web = True
-    # # while loop to ensure user input is "y" or "n"
-    # while web is True:
-    #     # pylint: disable=input-builtin
-    #     visit_web = input("Would you like to view the data on the web?(y/n)")
-    #     if visit_web == "y":
-    #         # print(web_interface.web_interface())
-    #         print("link")
-    #         web = False
-    #     elif visit_web == "n":
-    #         data_collection.print_file("contributor_data", ["repo"])
-    #         # exits loop when user chooses not to use the web interface
-    #         web = False
-    #     else:
-    #         print("Please enter (y/n).")
-    #         # loop is repeated when output is not "y" or "n"
-    # print("Thank you for using GatorCogitate. Have a great day!")
-    # end of the CLI main method
+    if args["web"] is True:
+        # print(web_interface.web_interface())
+        print("'web Link'")
+    elif args["web"] is False:
+        print("Printing data in terminal...\nTo see the output in"
+              + "the web, simply add '-w yes' to your command line arguments")
+        # Temporary structure given issue retrieval is the only function
+        contributor_data = data_collection.initialize_contributor_data(
+            "contributor_data_template"
+        )
+        contributor_data = data_collection.retrieve_issue_data(
+            repository, args["state"], contributor_data
+        )
 
 
 def retrieve_arguments():
@@ -73,13 +53,15 @@ def retrieve_arguments():
         "--state",
         required=False,
         type=str,
+        default="both",
         help="State of the Issue, open or closed",
     )
     a_parse.add_argument(
         "-w",
         "--web",
-        required=True,
+        required=False,
         type=bool_validator,
+        default=False,
         help="Whether to show the detailed result in web interface",
     )
 
