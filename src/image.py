3aa265a6c7dc910a316c095ae8826d2e56f0f52b
clussier <lussierc@allegheny.edu>
2020-03-12 20:52:08 -0400

    Add captions for homepage

--- src/image.py
+++ src/image.py
@@ -3,7 +3,10 @@ import numpy as np
 import pandas as pd
 
 from PIL import Image
-image = Image.open('../images/logo.png')
+image = Image.open('./images/logo.png')
 
-st.image(image, caption='Welcome',
+st.image(image,
          use_column_width=True)
+
+st.title("Welcome to Cogitate!")
+st.text("Use the sidebar on the left to navigate through Cogitate's features.")
