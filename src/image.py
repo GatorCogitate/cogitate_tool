c29b0ecc94eb6d3d1878972353c78fc9ac7e2bde
devin spitalny <devinspitalny@devins-MacBook-Air.local>
2020-03-12 20:45:44 -0400

    Creating home page image

--- src/image.py
+++ src/image.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from PIL import Image
-image = Image.open('logo.png')
+image = Image.open('../images/logo.png')
 
 st.image(image, caption='Welcome',
          use_column_width=True)
