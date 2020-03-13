29f93b5ed42b47bbfa066cdd2d64a0704ad59aff
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2020-03-12 21:10:15 -0400

    Add web_int test

--- tests/test_web_interface.py
+++ tests/test_web_interface.py
@@ -5,3 +5,17 @@ import pytest
 from src import data_collection
 from src import json_handler
 from src import web_interface
+
+
+
+
+
+
+
+
+
+
+def test_graph_commits_by_individual():
+    """Checks that the size of the input variable is correct."""
+    df = web_interface.graph_commits_by_individual(dict)
+    assert df != 0
