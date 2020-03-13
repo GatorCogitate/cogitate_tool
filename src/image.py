f4d2a93b1cdf6ddf6782002185daf895d16bb8a5
devin spitalny <devinspitalny@devins-MacBook-Air.local>
2020-03-12 20:07:16 -0400

    Fix indenting error in homepage

--- src/image.py
+++ src/image.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-    from PIL import Image
-    image = Image.open('logo.png')
+from PIL import Image
+image = Image.open('logo.png')
 
-    st.image(image, caption='Welcome',
-          use_column_width=True)
+st.image(image, caption='Welcome',
+         use_column_width=True)
